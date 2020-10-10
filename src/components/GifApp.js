c1e088698950c827e05c668a2e7db84c6d44aca2
alan <alanarostegui@hotmail.com>
2020-10-10 10:59:33 -0500

    initial commit

---
+++ src/components/GifApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GifApp = (props) => {
+  return (
+    <div>
+      <h2>Gif</h2>
+      <hr />
+    </div>
+  );
+};
+
+GifApp.propTypes = {};
+
+export default GifApp;
